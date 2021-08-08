15818c33079b8994eda0915bd2ee2bb510cfe96b
s-li1 <stevenli8892@hotmail.com.au>
2021-08-08 11:40:02 +1000

    create product category model

---
+++ server/Models/ProductCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace server.Models
+{
+    public class ProductCategory
+    {
+        public int ProductCategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
