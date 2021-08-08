478fa0eb52f0cc2418234e05b940c9b84fd7a40b
s-li1 <stevenli8892@hotmail.com.au>
2021-08-08 11:26:52 +1000

    create product model

---
+++ server/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace server.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
